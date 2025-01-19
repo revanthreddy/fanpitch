2a8fce16a54adc5afcb4443c19c20fc4317305b1
revanth <revanth.reddy@gmail.com>
2025-01-18 23:45:03 -0600

    first commit

---
+++ config.ts
@@ -0,0 +1,16 @@
+export const firebaseConfig = {
+  // Your web app's Firebase configuration here
+  // See https://firebase.google.com/docs/web/setup#add-sdks-initialize
+  apiKey: "AIzaSyD2jfPgzI9xevJAgmdpQyZ-jJQe653R9Rc",
+  authDomain: "gcp-mlb-atx-2025.firebaseapp.com",
+  projectId: "gcp-mlb-atx-2025",
+  storageBucket: "gcp-mlb-atx-2025.firebasestorage.app",
+  messagingSenderId: "1085313755214",
+  appId: "1:1085313755214:web:5e1bf857dbbedc00f6fb1f",
+  measurementId: "G-DZHW5NPK4J"
+};
+
+  // Your ReCAPTCHA Enterprise site key (must be from the same project
+  // as the Firebase config above).
+export const RECAPTCHA_ENTERPRISE_SITE_KEY =
+  "6LfIn7sqAAAAAFbUeADs2hEewZGTXKw9pRr03EsV";
